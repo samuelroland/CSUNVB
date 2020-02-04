984287ccd244252d100093acef4fe6de88e9ba8e
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-02-04 16:55:58 +0100

    drugHome.php Wip début du tableau html

--- view/drugHome.php
+++ view/drugHome.php
@@ -5,6 +5,18 @@ $title = "CSU-NVB - Stupéfiants";
 <div class="row m-2">
     <h1>Stupéfiants</h1>
 </div>
+<!-- Tableau -->
+<table class="table table-striped table-bordered">
+    <thead>
+    <tr>
+        <th colspan="2"><img src="" alt="Logo médicaments"></th>
+    </tr>
+
+    </thead>
+
+</table>
+
+
 <?php
 $content = ob_get_clean();
 require "gabarit.php";
