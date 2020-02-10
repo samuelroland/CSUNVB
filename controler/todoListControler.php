af1eb5ce9a76c9a824ce9806d6b148261a806ef3
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2020-02-10 13:55:27 +0100

    la correction a vraiment été corrigé

--- controler/todoListControler.php
+++ controler/todoListControler.php
@@ -34,43 +34,12 @@ function addNewToDo()
         $createtodoList[] = ["id" => $id, "date" => $date, "base" => $base, "nightjob" => $nightjob, "description" => $description, "" => $ , "type" => $type, "value" => $value];
         getTodoListItem($item);
     }
-    require_once '';*/
-}
+    require_once '';
+}*/
 ?>
-"id": 101,
-"date": "2020-02-03",
-"base": "Payerne",
-"nightjob": 0,
-"description": "Fax 144  Transmission",
-"acknowledged_by": null,
-"type": 0,
-"value": null
 
 
 
 
 
 
-
-function trySignin()
-{
-if (isset($_POST["user"]) && isset($_POST["password"])  != "" && $_POST["user"] != "" && $_POST["password"] != "") {
-
-
-$liste = getLogs();
-$Lastid = 0;
-foreach ($liste as $user) {
-$id = $user["id"];
-
-if ($id > $Lastid) {
-$Lastid = $id;
-}
-}
-$Lastid++;
-$liste[] = ["id" => $Lastid, "user" => $_POST["user"], "password" => $_POST["password"]];
-createLogs($liste);
-}
-require_once 'view/Signin.php';
-$_POST["user"] = null;
-unset($_POST["password"]);
-}
