9a9ef075c5e5389f659cdf9d151a2036e9a0564e
dmitrimeili <47849645+dmitrimeili@users.noreply.github.com>
2020-02-10 15:32:35 +0100

    connection login

--- controler/loginControler.php
+++ controler/loginControler.php
@@ -8,30 +8,39 @@
 require_once 'model/loginModel.php';
 
 
-function tryLogin($username,$password)
+function tryLogin($username, $password)
 {
     $UserLog = getUser($username);
-    if ($UserLog != '')
-    {
-        if ($UserLog['password'] == $password)
-        {
+    if ($UserLog != '') {
+        if ($UserLog['password'] == $password) {
+            if ($_SESSION["username"] == "admin") {
+
+                 $_SESSION['username'] = $UserLog['username'];
+                require_once "view/home.php";
+                echo "<a class='text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center' href='?action=admin'>Administration</a>";
+            }
+                else{
+                    $_SESSION['username'] = $UserLog['username'];
+                    require_once "view/home.php";
+                }
+            }
             $_SESSION['username'] = $UserLog['username'];
             require_once "view/home.php";
-        }else
-        {
+        } else {
             login();
         }
-    }else
-        {
-            login();
-        }
+
 }
+
 function login()
 {
     require_once 'view/login.php';
 }
-function diconnect(){
+
+function diconnect()
+{
     session_unset();
     require_once "view/home.php";
 }
+
 ?>
--- view/home.php
+++ view/home.php
@@ -3,7 +3,7 @@ ob_start();
 $title = "CSU-NVB - Accueil";
 ?>
 <div class="row m-2">
-    <a hidden class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center " href="?action=admin">Administration</a>
+
     <a class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" href="?action=shiftend">Remise de garde</a>
     <a class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" href="?action=todolist">Tâches hebdomadaires</a>
     <a class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" href="?action=drugs">Stupéfiants</a>
