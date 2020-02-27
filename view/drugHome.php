b2bce672b840abcfd340f75de62834a5179633dd
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-02-27 11:25:23 +0100

    drugHome changement 3 tableaux, bug text vertical, aligncenter text

--- view/drugHome.php
+++ view/drugHome.php
@@ -13,7 +13,7 @@ $title = "CSU-NVB - Stupéfiants";
 
 
 <!-- Morphine -->
-<table class="table table-striped table-bordered col-1">
+<table class="table table-striped table-bordered col-1 aligncenter">
     <thead>
     <tr>
         <th colspan="2"></th>
@@ -218,18 +218,8 @@ $title = "CSU-NVB - Stupéfiants";
 
 
 <!-- Fentanyl -->
-<table class="table table-striped table-bordered col-1">
+<table class="table table-striped table-bordered col-1 aligncenter">
     <thead>
-    <tr>
-        <th colspan="2"></th>
-        <th colspan="5">lundi 10 févr 20</th>
-        <th colspan="5">mardi 11 févr 20</th>
-        <th colspan="5">mercredi 12 févr 20</th>
-        <th colspan="5">jeudi 13 févr 20</th>
-        <th colspan="5">vendredi 14 févr 20</th>
-        <th colspan="5">samedi 15 févr 20</th>
-        <th colspan="5">dimanche 15 févr 20</th>
-    </tr>
     <tr>
         <th colspan="2" rowspan="2" class="imgheadertablezone"><img src="/model/dataStorage/img/logo-stups.png"
                                                                     alt="stups logo" class="imgheadertable"></th>
@@ -422,18 +412,8 @@ $title = "CSU-NVB - Stupéfiants";
 </table>
 
 <!-- Temesta -->
-<table class="table table-striped table-bordered col-1">
+<table class="table table-striped table-bordered col-1 aligncenter">
     <thead>
-    <tr>
-        <th colspan="2"></th>
-        <th colspan="5">lundi 10 févr 20</th>
-        <th colspan="5">mardi 11 févr 20</th>
-        <th colspan="5">mercredi 12 févr 20</th>
-        <th colspan="5">jeudi 13 févr 20</th>
-        <th colspan="5">vendredi 14 févr 20</th>
-        <th colspan="5">samedi 15 févr 20</th>
-        <th colspan="5">dimanche 15 févr 20</th>
-    </tr>
     <tr>
         <th colspan="2" rowspan="2" class="imgheadertablezone"><img src="/model/dataStorage/img/logo-stups.png"
                                                                     alt="stups logo" class="imgheadertable"></th>
