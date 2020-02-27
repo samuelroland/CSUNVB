2725c4ef594ccb089e7ac8f87e0b0c483a16fab5
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-02-27 08:06:30 +0100

    drugHome tableau design restructuration wip

--- view/drugHome.php
+++ view/drugHome.php
@@ -8,95 +8,64 @@ $title = "CSU-NVB - Stupéfiants";
 <!-- Tableau -->
 
 <h3>Contrôle des stupéfiants Hebdomadaire</h3>
-<h1>Semaine N 51</h1>
-<h2><em>Pour lundi 1 février 2020</em></h2>
-<br>
-<h3>Jours de la semaine 51</h3>
-<div id="divJoursSemaine" class="">
-    <a href="/?action=drugs&day=1" class="jourSemaine">
-        <button class="jourSemaine">Lundi</button>
-    </a>
-    <a href="/?action=drugs&day=1" class="jourSemaine">
-        <button class="jourSemaine">Mardi</button>
-    </a>
-    <a href="/?action=drugs&day=1" class="jourSemaine">
-        <button class="jourSemaine">Mercredi</button>
-    </a>
-    <a href="/?action=drugs&day=1" class="jourSemaine">
-        <button class="jourSemaine">Jeudi</button>
-    </a>
-    <a href="/?action=drugs&day=1" class="jourSemaine">
-        <button class="jourSemaine">Vendredi</button>
-    </a>
-    <a href="/?action=drugs&day=1" class="jourSemaine">
-        <button class="jourSemaine">Samedi</button>
-    </a>
-    <a href="/?action=drugs&day=1" class="jourSemaine">
-        <button class="jourSemaine">Dimanche</button>
-    </a>
-</div>
-<br>
+<h3>Semaine N 51</h3>
+
+
 
 <!-- Morphine -->
-<table class="table table-striped table-bordered">
+<table class="table table-striped table-bordered col-1">
     <thead>
     <tr>
+        <th colspan="7">lundi 10 févr 20</th>
+    </tr>
+    <tr>
         <th colspan="2" rowspan="2" class="imgheadertablezone"><img src="/model/dataStorage/img/logo-stups.png"
-                                                                    alt="stups logo" class="imgheadertable"></th>
+                                                                            alt="stups logo" class="imgheadertable"></th>
         <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th colspan="8">Numéros des véhicules</th>
+        <th colspan="3">Numéros des véhicules</th>
+        <th rowspan="2" class="txtvertical">Pharmacie</th>
     </tr>
     <tr>
         <th>35</th>
         <th>45</th>
         <th>12</th>
-        <th>15</th>
-        <th>31</th>
-        <th>94</th>
-        <th>09</th>
-        <th>35</th>
     </tr>
     </thead>
     <tbody>
     <tr>
         <td colspan="2" class=""><strong>Morphine N</strong></td>
-        <td></td>
-        <td>3</td>
-        <td>7</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>X</td>
+        <td>3-3</td>
+        <td>7-7</td>
+        <td>3-3</td>
+        <td>X</td>
+
     </tr>
     <tr>
         <td colspan="2" class="">12345</td>
         <td>4</td>
+        <td></td>
+        <td></td>
+        <td>1</td>
         <td>3</td>
-        <td>7</td>
-        <td>5</td>
-        <td>5</td>
-        <td>7</td>
-        <td>12</td>
-        <td>15</td>
-        <td>13</td>
+
     </tr>
     <tr>
         <td colspan="2" class="">13664</td>
         <td>4</td>
-        <td>3</td>
-        <td>7</td>
-        <td>5</td>
-        <td>5</td>
-        <td>7</td>
-        <td>12</td>
-        <td>15</td>
-        <td>13</td>
+        <td></td>
+        <td></td>
+        <td>2</td>
+        <td>2</td>
+
     </tr>
     </tbody>
 
 </table>
+
+
+
+
 
 <!-- Fentanyl -->
 
