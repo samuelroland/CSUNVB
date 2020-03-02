afda87c07a6c7a0595b7634ffec1d92a5116debb
Miguel Soares <miguel.soares@cpnv.ch>
2020-03-02 14:26:37 +0100

    ajout boutons semaine et select site

--- view/todoHomePage.php
+++ view/todoHomePage.php
@@ -10,39 +10,27 @@ ob_start();
 </div>
 <table class=" table table-striped">
     <div class="navbar nav-pills">
+        <th>
+            <select class="custom-select" id="Sites">
+                <option value="Saint-Loup">Saint-Loup</option>
+                <option value="Payerne">Payerne</option>
+                <option value="Yverdon">Yverdon</option>
+                <option value="La Valée-de-Joux">La Valée-de-Joux</option>
+                <option value="Saite-Croix">Saite-Croix</option>
 
-
-
-    </th>
+            </select>
+        </th>
     </div>
 </table>
 <div class="navbar nav-pills">
-
-    <a href="?action=todolisthome" ><button class="btn btn-info">Semaine 1</button></a>
-
+    <?php
+    for ($i = 1; $i < 52; $i++) {
+        echo "<div><a href='?action=todolisthome' ><button class='btn btn-info'>Semaine n° $i</button></a></div>";
+    }
+    ?>
 </div>
 
 
-<!--
-<div class="">
-    <div class="week">
-        <div class="">
-<?php
-/*
-foreach ($jours as $jour) {
-    echo "<span class=' dayheader' >" . $jour . "</span>";
-    echo "</div>";
-}
-echo "</div>";
-
-
-    foreach ($taches as $tache) {
-        echo "<span class=' hour'>" . $tache . "</span>";
-
-    }
-*/
-?>
-</div>-->
 <?php
 $content = ob_get_clean();
 require "gabarit.php";
