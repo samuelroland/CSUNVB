96375a78423e43e709aef8e2d917491ce56aac83
PARDO Christopher <Christopher.PARDO@cpnv.ch>
2020-03-02 16:01:25 +0100

    Début de la fonction de recupératin des feuilles

--- model/stupSheetModel.php
+++ model/stupSheetModel.php
@@ -4,3 +4,17 @@
   Project : 
   Date : 02.03.2020
 */
+
+function getAllSheets(){
+    //$goodArray = json_decode(file_get_contents("../model/dataStorage/drugs.json"), true); //Prend les éléments d'un fichier Json
+    $badArray = json_decode(file_get_contents("../model/dataStorage/stupSheets.json"), true); //Prend les éléments d'un fichier Json
+
+    //Ajoute une id aux différantes parties du tableau
+    foreach ($badArray as $p) {
+        $goodArray[$p["id"]] = $p;
+    }
+
+    return $goodArray; //Retourne le tableau indexé avec ses id
+}
+
+?>
