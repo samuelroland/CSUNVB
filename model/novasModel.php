32f414ac668737f80aa142b6b6a82fe87d2a2b2d
PARDO Christopher <Christopher.PARDO@cpnv.ch>
2020-03-05 12:14:45 +0100

    Ajout fonctions CRUD Novas

---
+++ model/novasModel.php
@@ -0,0 +1,87 @@
+<?php
+/*
+  Author : Christopher Pardo
+  Project : 
+  Date : 05.03.2020
+*/
+
+function getAllNovas(){
+    $badArray = json_decode(file_get_contents("model/dataStorage/Novas.json"), true); //Prend les éléments d'un fichier Json
+
+    //Ajoute une id aux différantes parties du tableau
+    foreach ($badArray as $p) {
+        $goodArray[$p["id"]] = $p;
+    }
+
+    return $goodArray; //Retourne le tableau indexé avec ses id
+}
+
+function getANova($number){
+    $novas = getAllNovas(); //Récupère les Drogues
+
+    foreach ($novas as $nova) {
+        if ($nova["number"] == $number){
+            return $nova;
+        }
+    }
+}
+
+function saveNovas($items)
+{
+    file_put_contents("model/dataStorage/Novas.json", json_encode($items)); //Écrit les éléments d'une variable dans un fichier Json
+}
+
+/**
+ * Modifie un item précis
+ * Le paramètre $item est un item complet (donc un tableau associatif)
+ * ...
+ */
+
+function updateANova($NovaToUpdate)
+{
+
+    $items = getAllNovas();
+
+    foreach ($items as $item){
+        if ($item["id"] == $NovaToUpdate["id"]){
+            $item = array_merge($item, $NovaToUpdate);
+            $items[$item["id"]] = $item;
+        }
+    }
+
+    saveNovas($items);
+}
+
+/**
+ * Ajoute un nouvel item
+ * Le paramètre $item est un item complet (donc un tableau associatif), sauf que la valeur de son id n'est pas valable
+ * puisque le modèle ne l'a pas encore traité
+ * ...
+ */
+function addANova($newNova)
+{
+    $items = getAllNovas();
+    $test = 0;
+    foreach ($items as $item){
+        if ($item["id"] > $test){
+            $test = $item["id"];
+        }
+    }
+
+    $id = $test + 1;
+    $newNova =  array_merge( ["id" => $id], $newNova);
+    $items[] = $newNova;
+
+    saveNovas($items);
+}
+
+function delANova($id)
+{
+    $items = getAllNovas();
+
+    unset($items[$id]);
+
+    saveNovas($items);
+}
+
+?>
