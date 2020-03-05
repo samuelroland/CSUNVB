8d1fb8c8f00da63c0042b6e6950dd4421667bee5
Miguel Soares <miguel.soares@cpnv.ch>
2020-03-05 12:14:28 +0100

    test sites

--- view/todoHomePage.php
+++ view/todoHomePage.php
@@ -12,11 +12,11 @@ ob_start();
     <div class="navbar nav-pills">
         <th>
             <select class="custom-select" id="Sites">
-                <option value="3">Saint-Loup</option>
-                <option value="4">Payerne</option>
-                <option value="1">Yverdon</option>
-                <option value="5">La Valée-de-Joux</option>
-                <option value="2">Saite-Croix</option>
+                <option value="3"<?php if($_SESSION['user'][3]['id'] == 3) echo 'selected';?>>Saint-Loup</option>
+                <option value="4"<?php if($_SESSION['user'][4]['id'] == 4) echo 'selected';?>>Payerne</option>
+                <option value="1"<?php if($_SESSION['user'][1]['id'] == 1) echo 'selected';?>>Yverdon</option>
+                <option value="5"<?php if($_SESSION['user'][5]['id'] == 5) echo 'selected';?>>La Valée-de-Joux</option>
+                <option value="2"<?php if($_SESSION['user'][2]['id'] == 2) echo 'selected';?>>Saite-Croix</option>
 
             </select>
         </th>
