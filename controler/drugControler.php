9e58bd86b0588293f0125f4079d56a4dfc63a7f2
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-03-09 15:02:46 +0100

    controleur stups get data for use

--- controler/drugControler.php
+++ controler/drugControler.php
@@ -6,14 +6,22 @@
  **/
 
 require_once 'model/drugModel.php';
+require_once 'model/basesModel.php';
 
 function drugdetails()
 {
+    $drugs = getAllDrugs();
+    $bases = getAllBases();
+    $novas = getAllNovas();
+    $stupsheets = getAllSheets();
+    $batches = getAllBatches();
     require_once 'view/drugsDetails.php';
 }
 
 function drugHomePage($week, $base)
 {
+    $bases = getAllBases();
+
     require_once 'view/drugsHome.php';
 }
 
