1054c9d63764b449ab694a8721b3f295aecd9e4f
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2020-03-12 11:23:42 +0100

    les tests effectué pour la semaine et pour les tâches sont bons

--- unitTest/todoListTest.php
+++ unitTest/todoListTest.php
@@ -6,7 +6,9 @@
 */
 require 'model/todoListModel.php';
 
-echo"Test read: ";
+/** ------------------TASK---------------------- */
+
+echo"Test read task: ";
 $tasktestread = readTodoListTaskById(7);
 if($tasktestread['id'] == 7 && $tasktestread['type'] == 0 && $tasktestread['daything'] == 1 && $tasktestread['description'] == "Fax 144 Transmission" && $tasktestread['display_order'] == null) {
     echo" OK\n";
@@ -16,13 +18,13 @@ else{
 }
 
 
-echo"Test create: ";
+echo"Test create task: ";
 // $task = ["id" => $id, "type" => $type, "daything" => $daything, "description" => $description, "display_order": null];
 $task = ["id" => null, "type" => 1, "daything" => 1, "description" => "C'est un test", "display_order" => null];
-$idgiven = createTodoListTask($task);
+$idgiventask = createTodoListTask($task);
 $tasks = getTodoListTasks();
 $counttasks = count($tasks);
-$id = $idgiven;
+$id = $idgiventask;
 if(readTodoListTaskById($id) != null) {
     echo " OK\n";
     $counttasks = count($tasks);
@@ -32,7 +34,7 @@ else {
 }
 
 // compte delete recompte
-echo "Test delete: ";
+echo "Test delete task: ";
 $tasks = getTodoListTasks();
 $counttasksbefore = count($tasks);
 $id = 5;
@@ -47,11 +49,63 @@ if ($tasktestdelete == null) {
 }
 
 
-echo"Test update: ";
+echo"Test update task: ";
 $tasktestupdate = readTodoListTaskById(6);
 $task = ["id" => 6, "type" => 1, "daything" => 1, "description" => "Test upgrade", "display_order" => null];
 updateTodoListTask($task);
 if($tasktestupdate['id'] == 6 && $tasktestupdate['type'] == 1 && $tasktestupdate['daything'] == 1 && $tasktestupdate['description'] == "Test upgrade" && $tasktestupdate['display_order'] == null) {
+    echo" OK\n";
+}
+else{
+    echo"BUG\n";
+}
+
+/** ------------------WEEK---------------------- */
+
+echo"Test read week: ";
+$weektestread = readTodoListWeekById(2);
+if($weektestread['id'] == 2 && $weektestread['week'] == 2009 && $weektestread['state'] == 'closed' && $weektestread['base_id'] == 1) {
+    echo" OK\n";
+}
+else{
+    echo"BUG\n";
+}
+
+echo"Test create week: ";
+// $task = ["id" => $id, "week" => $week, "state" => $state, "base_id" => $base_id];
+$week = ["id" => null, "week" => 2013, "state" => "closed", "base_id" => 4];
+$idgivenweek = createTodoListWeek($week);
+$weeks = getTodoListWeeks();
+$countweeks = count($weeks);
+$id = $idgivenweek;
+if(readTodoListWeekById($id) != null) {
+    echo " OK\n";
+    $countweeks = count($weeks);
+}
+else {
+    echo"BUG\n";
+}
+
+// compte delete recompte
+echo "Test delete week: ";
+$tasks = getTodoListWeeks();
+$countweeksbefore = count($weeks);
+$id = 7;
+destroyTodoListWeek($id);
+$weektestdelete = readTodoListWeekById($id);
+$countweeksbafter = count($weeks);
+
+if ($weektestdelete == null) {
+    echo " OK\n";
+} else {
+    echo "BUG\n";
+}
+
+echo"Test update week: ";
+$weektestupdate = readTodoListWeekById(20);
+$week = ["id" => 20, "week" => 2018, "state" => "open", "base_id" => 1];
+updateTodoListWeek($week);
+if($weektestupdate['id'] == 20 && $weektestupdate['week'] == 2018 && $weektestupdate['state'] == 'open' && $weektestupdate['base_id'] == 1) {
     echo" OK\n";
 }
 else{
