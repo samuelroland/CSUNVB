7b8bba5615cd89bcfe02530f7773887309eda919
Miguel Soares <47849626+miguelsoaresking500@users.noreply.github.com>
2020-03-16 11:49:31 +0100

    minimal changes

--- controler/Help.php
+++ controler/Help.php
@@ -1,10 +1,4 @@
 <?php
-require_once 'view/sheetselect.php';
-require_once 'model/drugModel.php';
-require_once 'model/basesModel.php';
-require_once 'model/novasModel.php';
-require_once 'model/stupSheetModel.php';
-require_once 'model/batchesModel.php';
 
 function SelectSheet ($mode) {
     switch ($mode) {
