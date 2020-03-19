106e0777847a03006ff74becd9de5fb31b7ccd8a
Luis Pedro Pinheiro <47849666+luispedropinheiro@users.noreply.github.com>
2020-03-19 12:18:03 +0100

    adminHome

--- view/adminHome.php
+++ view/adminHome.php
@@ -5,6 +5,8 @@ $title = "CSU-NVB - Administration";
 <div class="row m-2">
     <a class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" href="?action=crew">Secouristes</a>
     <a class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" href="?action=bases">Bases</a>
+    <a class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" href="?action=">Ambulances</a>
+    <a class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" href="?action=">Médicaments</a>
 </div>
 <?php
 $content = ob_get_clean();
