b34224f9dd2afdb02c6141c456ba0da49fd1b935
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-03-19 01:25:41 +0100

    Ajout fonction de recuperation de tout les chek et test

--- unitTest/pharmaCheksTest.php
+++ unitTest/pharmaCheksTest.php
@@ -6,73 +6,7 @@ print "Fonction de récupération des cheks : ";
 
 $allCheks = getAllCheks();
 
-if (count($allCheks) == 3){
-    print "OK\n";
-}
-else{
-    print "Pas OK\n";
-}
-
-print "Fonction de récupération d'un chek : ";
-
-$chek = getAChek(2);
-
-if ($chek["name"] == "Fentanyl"){
-    print "OK\n";
-}
-else{
-    print "Pas OK\n";
-}
-
-print "Fonction de ajout d'un chek : ";
-
-addAChek("test");
-
-$cheks = getAllCheks();
-
-$test = false;
-
-foreach ($cheks as $chek){
-    if ($chek["name"] == "test"){
-        $test = true;
-        break;
-    }
-}
-
-if($test == true){
-    print "OK\n";
-}
-else{
-    print "Pas OK\n";
-}
-
-print "Fonction de modification d'un chek : ";
-
-$chekToUpdate = [
-    "id" => 2,
-    "name" => "test1"
-];
-
-updateAChek($chekToUpdate);
-
-$cheks = getAllCheks();
-
-$test = false;
-
-if ($cheks["2"]["name"] == "test1"){
-    print "OK\n";
-}
-else{
-    print "Pas OK\n";
-}
-
-print "Fonction de supression d'un chek : ";
-
-delAChek(4);
-
-$cheks = getAllCheks();
-
-if (!isset($cheks[4])){
+if (count($allCheks) == 4){
     print "OK\n";
 }
 else{
