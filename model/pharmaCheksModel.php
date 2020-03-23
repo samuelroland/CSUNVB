a16f8f65086fa9425ab54f23a62d582b717ff03c
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-03-23 01:42:56 +0100

    Fonctions de CRUD complet de la feuilles de check

--- model/pharmaCheksModel.php
+++ model/pharmaCheksModel.php
@@ -16,4 +16,58 @@ function getAllCheks(){
     return $goodArray; //Retourne le tableau indexé avec ses id
 }
 
+function getAChek($date, $batch_id){
+    $cheks = getAllCheks();
+
+    foreach ($cheks as $chek) {
+        if ($chek["date"] == $date){
+            if ($chek["batch_id"] == $batch_id)
+                return $chek;
+        }
+    }
+}
+
+function saveCheks($items)
+{
+    file_put_contents("model/dataStorage/pharmachecks.json", json_encode($items)); //Écrit les éléments d'une variable dans un fichier Json
+}
+
+function addAChek($chek){
+    $items = getAllCheks();
+    $test = 0;
+    foreach ($items as $item){
+        if ($item["id"] > $test){
+            $test = $item["id"];
+        }
+    }
+
+    $id = $test + 1;
+    $chek =  array_merge( ["id" => $id], $chek);
+    $items[] = $chek;
+
+    saveCheks($items);
+}
+
+function updateAChek($chekToUpdate){
+    $items = getAllCheks();
+
+    foreach ($items as $item){
+        if ($item["id"] == $chekToUpdate["id"]){
+            $item = array_merge($item, $chekToUpdate);
+            $items[$item["id"]] = $item;
+        }
+    }
+
+    saveCheks($items);
+}
+
+function delAChek($id)
+{
+    $items = getAllCheks();
+
+    unset($items[$id]);
+
+    saveCheks($items);
+}
+
 ?>
--- unitTest/pharmaCheksTest.php
+++ unitTest/pharmaCheksTest.php
@@ -6,10 +6,71 @@ print "Fonction de récupération des cheks : ";
 
 $allCheks = getAllCheks();
 
-if (count($allCheks) == 4){
+if (count($allCheks) == 4) {
     print "OK\n";
+} else {
+    print "Pas OK\n";
 }
-else{
+
+print "Fonction de récupération d'un ckek : ";
+
+$chek = getAChek("2020-03-09", 2);
+
+if ($chek["date"] == "2020-03-09" && $chek["batch_id"] == 2) {
+    print "OK\n";
+} else {
+    print "Pas OK\n";
+}
+
+print "Fonction d'ajout d'un chek : ";
+
+$newChek = [
+    "date" => "2020-03-23",
+    "start" => 5,
+    "end" => 3,
+    "batch_id" => 2,
+    "user_id" => 2,
+    "stupsheet_id" => 4
+];
+
+addAChek($newChek);
+
+$chek = getAChek($newChek["date"], $newChek["batch_id"]);
+
+if ($chek["start"] == $newChek["start"] && $chek["end"] == $newChek["end"]) {
+    print "OK\n";
+} else {
+    print "Pas OK\n";
+}
+
+print "Fonction de modification d'un check : ";
+
+$chekToUpdate = [
+    "id" => 2,
+    "end" => 20
+];
+
+updateAChek($chekToUpdate);
+
+$cheks = getAllCheks();
+
+$test = false;
+
+if ($cheks[2]["end"] == $chekToUpdate["end"]) {
+    print "OK\n";
+} else {
+    print "Pas OK\n";
+}
+
+print "Fonction de suppression d'un chek : ";
+
+delAChek(4);
+
+$cheks = getAllCheks();
+
+if (!isset($cheks[4])){
+    print "OK\n";
+} else {
     print "Pas OK\n";
 }
 
