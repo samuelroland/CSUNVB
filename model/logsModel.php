be84ec2a8b2c4fe7bad834622f93929feefa299d
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-03-26 01:15:56 +0100

    Ajout CRUD logs

---
+++ model/logsModel.php
@@ -0,0 +1,73 @@
+<?php
+
+function getAllLogs(){
+    $badArray = json_decode(file_get_contents("model/dataStorage/logs.json"), true); //Prend les éléments d'un fichier Json
+
+    //Ajoute une id aux différantes parties du tableau
+    foreach ($badArray as $p) {
+        $goodArray[$p["id"]] = $p;
+    }
+
+    return $goodArray; //Retourne le tableau indexé avec ses id
+}
+
+function getALog($id){
+    $logs = getAllLogs(); //Récupère les logs
+
+    foreach ($logs as $log) {
+        if ($log["id"] == $id){
+            return $log;
+        }
+    }
+}
+
+function saveLogs($items)
+{
+    file_put_contents("model/dataStorage/logs.json", json_encode($items)); //Écrit les éléments d'une variable dans un fichier Json
+}
+
+function addALog($newLog)
+{
+    $items = getAllLogs();
+    $test = 0;
+    foreach ($items as $item){
+        if ($item["id"] > $test){
+            $test = $item["id"];
+        }
+    }
+
+    date_default_timezone_set('Europe/Zurich');
+    $timestamp = date("Y-m-j G:i:s");
+
+    $id = $test + 1;
+    $newLog =  array_merge( ["id" => $id, "timestamp" => $timestamp], $newLog);
+    $items[] = $newLog;
+
+    saveLogs($items);
+}
+
+function updateALog($logToUpdate)
+{
+
+    $items = getAllLogs();
+
+    foreach ($items as $item){
+        if ($item["id"] == $logToUpdate["id"]){
+            $item = array_merge($item, $logToUpdate);
+            $items[$item["id"]] = $item;
+        }
+    }
+
+    saveLogs($items);
+}
+
+function delALog($id)
+{
+    $items = getAllLogs();
+
+    unset($items[$id]);
+
+    saveLogs($items);
+}
+
+?>
---
+++ unitTest/logsTest.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once "model/logsModel.php";
+
+print "Fonction de récupération des logs : ";
+
+$allLogs = getAllLogs();
+
+if (count($allLogs) == 2){
+    print "OK\n";
+}
+else{
+    print "Pas OK\n";
+}
+
+print "Fonction de récupération d'une log : ";
+
+$log = getALog(1);
+
+if ($log["id"] == 1){
+    print "OK\n";
+}
+else{
+    print "Pas OK\n";
+}
+
+print "Fonction de ajout d'une log : ";
+
+$logToAdd = [
+    "author_id" => 12,
+    "item_type" =>  1,
+    "item_id" => 20,
+    "text" => "Test"
+];
+
+addALog($logToAdd);
+
+$logs = getAllLogs();
+
+$test = false;
+
+foreach ($logs as $log){
+    if ($log["text"] == $logToAdd["text"]){
+        $test = true;
+        break;
+    }
+}
+
+if($test == true){
+    print "OK\n";
+}
+else{
+    print "Pas OK\n";
+}
+
+print "Fonction de modification d'une log : ";
+
+$logToUpdate = [
+    "id" => 1,
+    "text"=> "test",
+];
+
+updateALog($logToUpdate);
+
+$logs = getAllLogs();
+
+$test = false;
+
+if ($logs[1]["text"] == "test"){
+    print "OK\n";
+}
+else{
+    print "Pas OK\n";
+}
+
+print "Fonction de supression d'une log : ";
+
+delALog(2);
+
+$logs = getAllLogs();
+
+if (!isset($logs[2])){
+    print "OK\n";
+}
+else{
+    print "Pas OK\n";
+}
+
+?>
