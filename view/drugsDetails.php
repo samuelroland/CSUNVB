2898d6fc837f15b276883bf4040f584171124295
samuelroland <samuel.roland@cpnv.ch>
2020-04-02 11:13:28 +0200

    drugsdetails prendre en compte nb de novas pour ne pas avoir de colonne vide

--- view/drugsDetails.php
+++ view/drugsDetails.php
@@ -43,11 +43,13 @@ $title = "CSU-NVB - Stupéfiants";
         <th colspan="2"></th>   <!-- cellule vide haut gauche du tableau -->
 
         <?php
+        $nbnovas = count($stupsheet['novas']);
+        $colspanforday = $nbnovas + 2;  //nombre de novas + 2 cases pharmacies
         foreach ($datesoftheweek as $onedate) {
             if (strcmp(date("Y-m-d", $onedate), date("Y-m-d")) == 0) {   //si la date est aujourdh'hui.
-                echo "<th class='bg-info' colspan='5'><img src='assets/images/today.png' class='icontoday' alt='icone jour actuel'>" . date("j F Y", $onedate) . "</th>";
+                echo "<th class='bg-info' colspan='$colspanforday'><img src='assets/images/today.png' class='icontoday' alt='icone pour le jour actuel'>" . date("j F Y", $onedate) . "</th>";
             } else {
-                echo "<th colspan='5'>" . date("j F Y", $onedate) . "</th>";
+                echo "<th colspan='$colspanforday'>" . date("j F Y", $onedate) . "</th>";
             }
         }
         ?>
@@ -55,44 +57,21 @@ $title = "CSU-NVB - Stupéfiants";
     <tr>
         <th colspan="2" rowspan="2" class="imgheadertablezone"><img src="/model/dataStorage/img/logo-stups.png"
                                                                     alt="stups logo" class="imgheadertable"></th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th colspan="3">Véhicules</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th colspan="3">Véhicules</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th colspan="3">Véhicules</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th colspan="3">Véhicules</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th colspan="3">Véhicules</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th colspan="3">Véhicules</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
-        <th colspan="3">Véhicules</th>
-        <th rowspan="2" class="txtvertical">Pharmacie</th>
+        <?php
+        for ($j = 0; $j < 7; $j++) {
+            ?>
+            <th rowspan="2" class="txtvertical">Pharmacie</th>
+            <th colspan="<?= $nbnovas ?>">Véhicules</th>
+            <th rowspan="2" class="txtvertical">Pharmacie</th>
+        <?php }
+        ?>
     </tr>
     <tr>
         <?php for ($i = 0; $i < 7; $i++) {
             foreach ($stupsheet["novas"] as $nova) {
                 echo "<th>$nova</th>";
             }
-            switch (count($stupsheet["novas"])) {
-                case 1 :
-                    echo "<th></th>";
-                    echo "<th></th>";
-                    break;
-                case 2 :
-                    echo "<th></th>";
-                    break;
-                default:    //si il y en a 3 par exemple, ne rien afficher.
-                    break;
-            }
+
         } ?>
     </tr>
     </thead>
@@ -100,11 +79,14 @@ $title = "CSU-NVB - Stupéfiants";
         <tbody>
         <tr class="bg-lightgrey">
             <td colspan="2" class=""><strong><?= $drug["name"] ?></strong></td>
-            <?php for ($f = 1; $f <= 7; $f++) { ?>
+            <?php for ($f = 1; $f <= 7; $f++) { //pour les 7 jours de la semaine.
+                ?>
                 <td>X</td>
-                <td>13-13</td>
-                <td>17-17</td>
-                <td>13-13</td>
+                <?php
+                foreach ($stupsheet["novas"] as $nova) {
+                    echo "<td>13-13</td>";  //la case pour novacheck
+                }
+                ?>
                 <td>X</td>
             <?php } ?>
         </tr>
@@ -113,31 +95,31 @@ $title = "CSU-NVB - Stupéfiants";
 
         as $batch) {  //pour chaque lot
         if ($batch["drug_id"] == $drug["id"] && in_array($batch['number'], $stupsheet['batches'])) {
-            switch ($batch['state']){
-                case "new":
-                    $CssClassForTheBatch = "bg-lightgreen";
-                    break;
-                case "inuse":
-                    $CssClassForTheBatch = "bg-lightblue";
-                    break;
-                case "used":
-                    $CssClassForTheBatch = "bg-violet";
-                    break;
+        switch ($batch['state']) {
+            case "new":
+                $CssClassForTheBatch = "bg-lightgreen";
+                break;
+            case "inuse":
+                $CssClassForTheBatch = "bg-lightblue";
+                break;
+            case "used":
+                $CssClassForTheBatch = "bg-violet";
+                break;
 
-            }
-            ?>
+        }
+        ?>
         <tr class="<?= $CssClassForTheBatch ?>">
-            <td colspan="2" class=""><?= $batch["number"] . " id:" . $batch['id'] ?> state=<?= $batch["state"]?></td>
+            <td colspan="2" class=""><?= $batch["number"] . " id:" . $batch['id'] ?> state=<?= $batch["state"] ?></td>
             <?php
             foreach ($datesoftheweek as $day) {  //pour chaque jour de la semaine
                 $foundacheck = false;   //si on a trouvé un check pour le jour en cours. par défaut à faux
                 foreach ($listofchecks as $check) { //pour chaque check
                     if ($check["batch_id"] == $batch["id"] && strtotime($check['date']) == $day) { //si c'est le bon batch et le jour en cours ?>
-                        <td><?= $sheet[$date][$nova_id][$batch_id] ?></td>
                         <td><?= $check["start"] ?> Checkid = <?= $check["id"] ?></td>
-                        <td class=""><?= date("d M", strtotime($check["date"])) ?></td>
-                        <td class=""><?= "stupsheetid = " . $check['stupsheet_id'] ?></td>
-                        <td class=""><?= $check["batch_id"] ?></td>
+                        <?php foreach ($stupsheet['novas'] as $nova) { ?>
+                            <td><?= $sheet[$date][$nova_id][$batch_id] ?></td>
+                        <?php } ?>
+
                         <td><?= $check["end"] ?></td>
                         <?php
                         $foundacheck = true;    //donc on a trouvé.
