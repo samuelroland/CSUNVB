51119c314b335e05aa298a8668ff1e9a92f69359
samuelroland <samuel.roland@cpnv.ch>
2020-04-09 14:03:23 +0200

    drugsdetails couleurs batches et données debug enlevées et état lot ajouté

--- view/drugsDetails.php
+++ view/drugsDetails.php
@@ -47,7 +47,7 @@ $title = "CSU-NVB - Stupéfiants";
         $colspanforday = $nbnovas + 2;  //nombre de novas + 2 cases pharmacies
         foreach ($datesoftheweek as $onedate) {
             if (strcmp(date("Y-m-d", $onedate), date("Y-m-d")) == 0) {   //si la date est aujourdh'hui.
-                echo "<th class='bg-info' colspan='$colspanforday'><img src='assets/images/today.png' class='icontoday' alt='icone pour le jour actuel'>" . date("j F Y", $onedate) . "</th>";
+                echo "<th class='bg-cell-today' colspan='$colspanforday'><img src='assets/images/today.png' class='icontoday' alt='icone pour le jour actuel'>" . date("j F Y", $onedate) . "</th>";
             } else {
                 echo "<th colspan='$colspanforday'>" . date("j F Y", $onedate) . "</th>";
             }
@@ -98,30 +98,33 @@ $title = "CSU-NVB - Stupéfiants";
         if ($batch["drug_id"] == $drug["id"] && in_array($batch['number'], $stupsheet['batches'])) {
         switch ($batch['state']) {
             case "new":
-                $CssClassForTheBatch = "bg-lightgreen";
+                $CssClassForTheBatch = "bg-batch-new";
+                $text = "Nouveau";
                 break;
             case "inuse":
-                $CssClassForTheBatch = "bg-lightblue";
+                $CssClassForTheBatch = "bg-batch-inuse";
+                $text = "En cours";
                 break;
             case "used":
-                $CssClassForTheBatch = "bg-violet";
+                $CssClassForTheBatch = "bg-batch-used";
+                $text = "Utilisé";
                 break;
 
         }
         ?>
-        <tr class="<?= $CssClassForTheBatch ?>">
-            <td colspan="2" class=""><?= $batch["number"] . " id:" . $batch['id'] ?> state=<?= $batch["state"] ?></td>
+        <tr>
+            <td colspan="2" class="<?= $CssClassForTheBatch ?>"><?= $batch["number"]?><br><?= $text ?></td>
             <?php
             foreach ($datesoftheweek as $day) {  //pour chaque jour de la semaine
                 $foundacheck = false;   //si on a trouvé un check pour le jour en cours. par défaut à faux
                 foreach ($listofchecks as $check) { //pour chaque check
                     if ($check["batch_id"] == $batch["id"] && strtotime($check['date']) == $day) { //si c'est le bon batch et le jour en cours ?>
-                        <td class="clickable" data-href="?action=updatePharmaCheck&batch_id=<?= $batch["id"] ?>&stupsheet_id=<?= $stupsheet["id"] ?>&date=<?= date("Y-m-d", $day) ?>"><?= $check["start"] ?> Checkid = <?= $check["id"] ?>"></td>
+                        <td class="clickable" data-href="?action=updatePharmaCheck&batch_id=<?= $batch["id"] ?>&stupsheet_id=<?= $stupsheet["id"] ?>&date=<?= date("Y-m-d", $day) ?>"><?= $check["start"] ?></td>
                         <?php foreach ($novas as $nova) { ?>
                             <td><?= $sheet[$date][$nova_id][$batch_id] ?></td>
                         <?php } ?>
 
-                        <td><?= $check["end"] ?></td>
+                        <td class="clickable" data-href="?action=updatePharmaCheck&batch_id=<?= $batch["id"] ?>&stupsheet_id=<?= $stupsheet["id"] ?>&date=<?= date("Y-m-d", $day) ?>"><?= $check["end"] ?></td>
                         <?php
                         $foundacheck = true;    //donc on a trouvé.
                     }
@@ -130,8 +133,9 @@ $title = "CSU-NVB - Stupéfiants";
                     echo "<td></td><td></td><td></td><td></td><td></td>";    //afficher 5 cases vides ne pas décaler les checks des jours suivants
                 }
             }
-            }
-            echo "<tr>";
+            echo "</tr>";   //fin de la ligne du batch
+            }   //fin du if
+
             } ?>
         </tbody>
     <?php }
