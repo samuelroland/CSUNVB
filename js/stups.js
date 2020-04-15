bbaf56c4cedf1b69da398e41d834d7c85564df3c
samuelroland <samuel.roland@cpnv.ch>
2020-04-15 22:30:44 +0200

    js files about sheetselect and stups: comments added and redondance deleted

--- js/stups.js
+++ js/stups.js
@@ -6,39 +6,6 @@
  * Date: Février 2020
  **/
 
-$(document).ready(function () {
-    $("label").on("click", function (lbl) { //si on clique sur un label
-        inpsheetid.value = $(lbl.target).data("sheetid");  //on sauve la valeur de sheetid dans l'input hidden inpsheetid
-        document.getElementById("sheetForm").submit();   //on envoit le formulaire
-    })
-})
-
-document.addEventListener("DOMContentLoaded", init) //une fois les éléments du DOM chargé on lance init pour déclarer les événements
-
-function init() {
-    document.getElementById("Sites").addEventListener("change", changestupssheets)
-    changestupssheets() //pour être sur que la liste de la bonne base est affichée
-}
-
-function changestupssheets() {  //changer l'affichage des blocs
-    //Changer le texte "Feuilles de la base xx" avec la base sélectionnée au dessus:
-    divListFeuilles.innerHTML = "Feuilles de la base de <strong>" + Sites.options[Sites.selectedIndex].text + "</strong>";
-
-    //INFO: les bases sont numérotés à partir de 0 (jusqu'à nombre de bases -1) et ces numéros ne sont pas en relation avec les id des bases. ce qui permet de simplifier l'affichage des blocs des bases.
-    //INFO: chaque base a un bloc div qui contient la liste des feuilles. chaque bloc est nommé divBasex où x est un nombre décrit au dessus.
-    baseid = Sites.selectedIndex    //prendre le numéro de la base sélectionnée
-
-    bloctodisplay = document.getElementById("divBase" + baseid)     //prendre le bloc de la base sélectionnée
-
-    //Cacher tous les blocs
-    for (i = 0; i < Sites.options.length; i++) {
-        document.getElementById("divBase" + i).classList.add("d-none")
-    }
-
-    //Afficher le bloc de la base sélectionnée
-    bloctodisplay.classList.remove("d-none")
-}
-
 document.addEventListener('DOMContentLoaded', function () {
     var els= document.getElementsByClassName("clickable");
     Array.prototype.forEach.call(els, function (el) {
@@ -47,4 +14,3 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     })
 })
-
